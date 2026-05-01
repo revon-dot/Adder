d0798c7a850549d33bd41196cdb511711ab6494a
revon-dot <theoldrevo@gmail.com>
2026-05-01 13:31:38 -0300

    Hide legacy ImgChest multi import action

--- views/editor-page.js
+++ views/editor-page.js
@@ -2,7 +2,6 @@ import { escapeHtml, attr } from "../utils.js";
 import { repoLabel, cubariUrlForPath } from "../repo.js";
 import { renderChapterCards } from "./editor-renderers.js";
 import { renderLanguageToggle, t } from "../i18n.js";
-import { multiChapterUploadButtonLabel } from "./multi-chapter-modal.js";
 
 function backIcon() {
   return `
@@ -94,7 +93,6 @@ export function renderEditorPage(current, manifest) {
       
       <div class="editor-chapter-actions">
         <button class="btn primary" id="add-chapter-btn">${label("Adicionar Capítulo", "Add Chapter")}</button>
-        <button class="btn ghost" id="multi-chapter-upload-btn">${multiChapterUploadButtonLabel()}</button>
       </div>
       
       <section class="panel" id="chapters-list">
